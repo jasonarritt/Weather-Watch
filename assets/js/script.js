1f1d387d728a563c919f5a8ffe981a3728a87bb3
Jason Arritt <jsn.arritt@gmail.com>
2022-04-01 18:32:19 -0400

    Search is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
 // OpenWeather API KEY: 4dbb43246a043fc03198de34a46d252d
 
+let searchTermEl = document.querySelector("#search-city");
+
+
+function retrieveSearchHistory() {
+
+}
+
 function fetchWeatherData(cityName) {
     let apiKey = "4dbb43246a043fc03198de34a46d252d"
     let apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + apiKey;
@@ -37,9 +44,9 @@ function fetchWeatherData(cityName) {
                 let currentWeatherPicture = data.weather[0].icon;
                 todayWeatherPictureEl.setAttribute("src", "https://openweathermap.org/img/wn/" + currentWeatherPicture + "@4x.png");
                 todayWeatherPictureEl.setAttribute("alt", data.weather[0].description);
+
+
                 
-
-
 
 
                 fetch(UVapiURL)
@@ -66,12 +73,42 @@ function fetchWeatherData(cityName) {
                         });
 
                     } else {
-                        alert("Error: Keyword did not return results");
                     }
                 })
                 .catch(function (error) {
-                    alert("Unable to connect to server");
                 });
+
+                let locationID = data.id;
+                let forecastApiURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + locationID + "&appid=" + apiKey;
+
+                fetch(forecastApiURL)
+                .then(function (response) {
+                    if (response.ok) {
+                        response.json().then(function (data) {
+                            console.log(data);
+
+                            let projectedForecastContainerEl = document.getElementById("projected-forecast");
+
+                            for (i = 1; i < 6; i++) {
+                                console.log(i);
+                                //TODO:Figure out forecast
+                                // let dayCardEl = document.createElement("div");
+                                // let dayCardDateEl = document.createElement("p");
+                                // let dayCardDate = new Date(data.list[i].dt * 1000);
+                                // dayCardDateEl.setAttribute("class", "mt-3 mb-0 forecast-date");
+                                // dayCardDateEl.text(dayCardDate)
+                                // console.log(dayCardDate);
+                            }
+
+
+                        });
+                    } else {
+                    }
+                })
+                .catch(function (error) {
+                });
+
+
 
             });
         } else {
@@ -83,5 +120,22 @@ function fetchWeatherData(cityName) {
     });
 }
 
+function formSubmitHandler(event) {
+    event.preventDefault();
+    let searchTerm = searchTermEl.value.trim();
+    console.log(searchTerm);
 
-fetchWeatherData("raleigh")
+    if (searchTerm) { 
+        fetchWeatherData(searchTerm);
+        searchFormEl.value = "";
+    } else {
+        alert("Please Enter the Name of a City to Search");
+    }
+}
+
+let searchFormEl = document.getElementById("city-search-form");
+
+searchFormEl.addEventListener("submit", formSubmitHandler);
+
+
+fetchWeatherData("san diego")
